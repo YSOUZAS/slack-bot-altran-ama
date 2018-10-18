5dbc67c8ac518e7b941f6c47bbbd6b31c68ab58e
SANTOS Yago Souza <yssantos@europe.corp.altran.com>
2018-10-18 18:02:13 +0100

    remove from start

--- index.js
+++ index.js
@@ -13,28 +13,26 @@ const bot = new SlackBot({
 bot.on('start', async () => {
 
     bot.postMessageToChannel("test-bot", `to na area`)
-
-    bot.on('message', async data => {
-        if (data.type !== 'message') {
-            return;
-        }
-
-        const user = await bot.getUserById(data.user);
-        const channel = await bot.getChannelById(data.channel);
-
-        if (user) {
-            console.log(channel)
-            console.log(user.profile.real_name_normalized);
-            const message = data.text;
-
-            if (message.toLowerCase().includes('boa noite'))
-                bot.postMessageToChannel(channel.name, `Boa Noite, ${user.profile.real_name_normalized}`)
-            if (message.toLowerCase().includes('bom dia'))
-                bot.postMessageToChannel(channel.name, `Bom Dia, ${user.profile.real_name_normalized}`)
-            if (message.toLowerCase().includes('boa tarde'))
-                bot.postMessageToChannel(channel.name, `Boa Tarde, ${user.profile.real_name_normalized}`)
-        }
-    });
-
 });
 
+bot.on('message', async data => {
+    if (data.type !== 'message') {
+        return;
+    }
+
+    const user = await bot.getUserById(data.user);
+    const channel = await bot.getChannelById(data.channel);
+
+    if (user) {
+        console.log(channel)
+        console.log(user.profile.real_name_normalized);
+        const message = data.text;
+
+        if (message.toLowerCase().includes('boa noite'))
+            bot.postMessageToChannel(channel.name, `Boa Noite, ${user.profile.real_name_normalized}`)
+        if (message.toLowerCase().includes('bom dia'))
+            bot.postMessageToChannel(channel.name, `Bom Dia, ${user.profile.real_name_normalized}`)
+        if (message.toLowerCase().includes('boa tarde'))
+            bot.postMessageToChannel(channel.name, `Boa Tarde, ${user.profile.real_name_normalized}`)
+    }
+});
